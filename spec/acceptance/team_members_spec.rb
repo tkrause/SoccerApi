09a913dd28971f33a8acf07d17f4fc80c8a5dac6
Thomas Krause <trueswordmaster@gmail.com>
2019-09-20 09:38:33 -0700

    Fix tests

--- spec/acceptance/team_members_spec.rb
+++ spec/acceptance/team_members_spec.rb
@@ -44,7 +44,6 @@ resource 'Team Members' do
         example_request 'successfully add team member' do
           expect(status).to eq(201)
           expect(json).to be_an_instance_of(Array)
-          # puts response_body
         end
       end
     end
